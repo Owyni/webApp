92cd67add1d81bc714bda348375d9fa09f9fce58
Owynn_ <angelaleman2212@gmail.com>
2024-10-06 10:32:43 -0600

    error 404 added, and some text corrections.

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def numero(edad):
 def mostrar_nombre(nombre):
     return render_template("mostrar.html", nombre_parametro = nombre)
 
+@app.errorhandler(404)
+def pagina_error(error):
+    return render_template("404.html", error=error), 404
+
 @app.route("/redirect")
 def mi_redirect():
     return redirect(url_for("hello_world"))
